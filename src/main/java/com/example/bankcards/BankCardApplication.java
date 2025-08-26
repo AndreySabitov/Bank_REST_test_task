67c81bea72ae986e40962daca7655acd43d1dc6f
Andrey Sabitov <andrey.sabitoff@yandex.ru>
2025-08-26 14:49:42 +0500

    Добавить Javadoc для главного класса приложения

--- src/main/java/com/example/bankcards/BankCardApplication.java
+++ src/main/java/com/example/bankcards/BankCardApplication.java
@@ -3,6 +3,12 @@ package com.example.bankcards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Точка входа в приложение.
+ * Запускает Spring Boot-контекст и выполняет инициализацию системы.
+ *
+ * @author Andrey Sabitov
+ */
 @SpringBootApplication
 public class BankCardApplication {
     public static void main(String[] args) {
